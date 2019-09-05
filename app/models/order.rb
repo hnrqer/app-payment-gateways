c5114bc6db4ef63fff5b0b627c48a758e0007e78
Henrique Sarmento <hnrqer@gmail.com>
2019-09-04 22:05:46 -0300

    Add order model

---
+++ app/models/order.rb
@@ -0,0 +1,16 @@
+class Order < ApplicationRecord
+  enum status: { pending: 0, failed: 1, paid: 2, paypal_executed: 3}
+  enum payment_gateway: { stripe: 0, paypal: 1 }
+  belongs_to :product
+  belongs_to :user
+
+  def set_paid
+    self.status = Order.statuses[:paid]
+  end
+  def set_failed
+    self.status = Order.statuses[:failed]
+  end
+  def set_paypal_executed
+    self.status = Order.statuses[:paypal_executed]
+  end
+end
