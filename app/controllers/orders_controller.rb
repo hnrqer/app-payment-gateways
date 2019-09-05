5fe2244768d06e55cfd4b30c208473c1ca9395d6
Henrique Sarmento <hnrqer@gmail.com>
2019-09-05 00:06:16 -0300

    Add index.html and and routes for index and submit
    The orders#index action returns two variables to be consumed
    in the front-end: @products_purchase and @products_subscription
    
    index.html.haml has a skeleton for the project. It has:
    * A list for purchases
    * A list for subscriptions
    
    Additionally, it has radio button to select Stripe or Paypal
    to perform payment and a submit button. A submit button is
    also implemented for each payment type as PayPal javascript
    library will add its own button design through its API.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,11 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
+    products = Product.all
+    @products_purchase = products.where(stripe_plan_name:nil, paypal_plan_name:nil)
+    @products_subscription = products - @products_purchase
   end
-  
+
   def submit
   end
 end
