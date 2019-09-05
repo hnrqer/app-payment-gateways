b9b2768fcd06e65b305d70ff805cd065e5490cc3
Henrique Sarmento <hnrqer@gmail.com>
2019-09-04 23:59:40 -0300

    Add submit route

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
   end
+  
+  def submit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/', to: 'orders#index'
+  post '/orders/submit', to: 'orders#submit'
 end
