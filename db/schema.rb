8bc4972373854a09ab2db5433042a8b6c60a22f9
Henrique Sarmento <hnrqer@gmail.com>
2019-09-07 17:37:00 -0300

    Add customer_id to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_004215) do
+ActiveRecord::Schema.define(version: 2019_09_07_202923) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "product_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_09_05_004215) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
