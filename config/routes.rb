1c64b3d58634ea808a30fa3d4992728bc9d4d60b
Henrique Sarmento <hnrqer@gmail.com>
2019-09-07 20:59:38 -0300

    Add routes for paypal create and execute purchases

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get '/', to: 'orders#index'
   post '/orders/submit', to: 'orders#submit'
+  post 'orders/paypal/create_payment'  => 'orders#paypal_create_payment', as: :paypal_create_payment
+  post 'orders/paypal/execute_payment'  => 'orders#paypal_execute_payment', as: :paypal_execute_payment
 end
